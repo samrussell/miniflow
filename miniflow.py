15da9ca91f31ae41606fef955644a0bcc3420b0c
Sam Russell <sam.h.russell@gmail.com>
2013-07-04 01:53:29 +1200

    building some structure around packet processing - keeping in mind building functions that we can through dummy packets at for unit testing

--- miniflow.py
+++ miniflow.py
@@ -6,16 +6,43 @@
 
 import socket
 import thread
+import struct
+
+def ProcessOpenFlowMessage(header, length, payload):
+    pass
+
+
+def OpenFlowHeader(data):
+    if(len(data) >=8):
+        return struct.unpack("!bbhl", data)
+    return None
+
+
+def GetOpenFlowMessage(clientsocket):
+    data = clientsocket.recv(8) # openflow standard says every message starts with 8-byte header
+    header = OpenFlowHeader(data)
+    # do a test for header == None and fail?
+    (version, ofpt_type, length, xid) = header
+    print "Version %d, type %d, length %d, xid %d" % (version, ofpt_type, length, xid)
+    # read rest of header
+    payload = clientsocket.recv(length-8)
+    ProcessOpenFlowMessage(header, length, payload)
+
 
 def OpenFlowServer(clientsocket, address):
     ipaddress = address[0]
     port = address[1]
     print "Got connection from %s:%d" % (ipaddress, port)
+    while 1:
+        data = GetOpenFlowMessage(clientsocket)
+        
+    
+    clientsocket.close()
 
 
 def Listen():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(("0.0.0.0", 6633))
+    serversocket.bind(("0.0.0.0", 6634))
     serversocket.listen(5)
     return serversocket
 
